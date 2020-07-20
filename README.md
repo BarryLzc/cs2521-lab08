628baddce11aeae1fc8cbc425cb1cb659e744fdd
z5212992 <z5212992@cse.unsw.edu.au>
2020-07-20 19:19:19 +1000

    added helloWorld.c

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cs2521-lab08
+That's my first time to using github
---
+++ helloWorld.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void){
+   
+   int a = 4/0; int b = 4/0;
+    if(a==b)
+    printf("%d",a)
+    
+    else printf("0");
+
+    return 0;
+}
